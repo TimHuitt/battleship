680af1002773ca78e90e8755f1a307e91bd9cc67
Tim Huitt <timhuitt@gmail.com>
2023-10-29 17:18:10 -0600

    add ship placement constraints

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 
 
 //*----- constants -----*//
+
+// list all player cells and thier state
 // 1 = occupied
 // 0 = empty
 // -1 = hit
-const playerState = {}
-const opponentState = {}
+const playerState = {
+  // active ships and their location
+  playerShips: {}
+}
 
+// list all opponent cells and thier state
+const opponentState = {
+  opponentShips: {}
+}
+
+// list of ship types and their cell length
 const ships = {
   destroyer: 2,
   sub: 3,
@@ -104,11 +114,13 @@ function renderBoard(e) {
 //*----- handlers -----*//
 
 function handleBoardEvent(e) {
+  if (e.type === 'click') {
+  }
   if (e.type === 'mouseover' && !e.target.id.includes('board')) {
-    handleCellHighlights(1, e)
+    renderCellHighlights(1, e)
   }
   if (e.type === 'mouseleave') {
-    handleCellHighlights(0, e)
+    renderCellHighlights(0, e)
   }
   
 }
@@ -139,17 +151,19 @@ function handleShipEvent(e) {
   }
 }
 
+//*----- rendering -----*//
+
 // row and column highlight on hover
 // type: 1/0 = add/remove
 // e = event
-function handleCellHighlights(type, e) {
+function renderCellHighlights(type, e) {
   const boardCells = document.querySelectorAll('.cell')
   const currentRow = e.target.id.split('')[0]
   const currentCol = e.target.id.slice(1)
 
   // type = 1: add row/col highlighting
   if (type) {
-    handleCellHighlights(0, e)
+    renderCellHighlights(0, e)
     boardCells.forEach((cell) => {
       if (cell.parentElement.id === currentPlayer &&
           (cell.id.includes(currentRow) ||
@@ -175,30 +189,67 @@ function handleCellHighlights(type, e) {
 
 //*----- setters -----*//
 
-placeShip('sub', 'a5', 'n')
 
-function placeShip(ship, cell, direction) {
+handleShipPlacement('carrier', 'j5', 'w')
+// handleShipPlacement('battleship', 'b8', 'n')
+// handleShipPlacement('cruiser', 'g4', 's')
+// handleShipPlacement('sub', 'i9', 'w')
+// handleShipPlacement('destroyer', 'f6', 'n')
+
+function handleShipPlacement(ship, cell, direction) {
   const currentCell = document.querySelector(`#${currentPlayer} #${cell}`)
   let currentRow = cell.split('')[0]
   let currentCol = parseInt(cell.slice(1))
-  let currentChar = currentRow.charCodeAt(0)
+  let currentChar = currentRow.charCodeAt(0) // a - j: 97 - 106
+  let valid = true
   let nextCellEl
   let nextCell
-  let currentLetter
-  
-  currentCell.style.backgroundColor = 'var(--blue)'
 
+  currentCell.classList.add('placed')
+  playerState[cell] = 1
+
+
+  if (direction === 'n' && currentChar - ships[ship] + 1 < 97) {
+    valid = false
+  }
+  if (direction === 'e' && currentCol + ships[ship] - 1 > 10) {
+    valid = false
+  }
+  if (direction === 's' && currentChar + ships[ship] - 1 > 106) {
+    valid = false
+  }
+  if (direction === 'w' && currentCol - ships[ship] + 1 < 1) {
+    valid = false
+  }
+
+  // add each new cell to board state 
+  // based on selected ship, start cell, and direction
   for (let i = 0; i < ships[ship] - 1; i++) {
-    if (direction === 'n') currentChar -= 1
-    if (direction === 'e') currentCol += 1
-    if (direction === 's') currentChar += 1
-    if (direction === 'w') currentCol -= 1
+    if (direction === 'n') {
+      if (currentChar > 97) {
+        currentChar -= 1
+      }
+    } else if (direction === 'e') {
+      if (currentCol < 10) {
+        currentCol += 1
+      }
+    } else if (direction === 's') {
+      if (currentChar + 1 <= 106) {
+        currentChar += 1
+      }
+    } else if (direction === 'w') {
+      if (currentCol - 1 >= 1) {
+        currentCol -= 1
+      }
+    }
 
     currentRow = String.fromCharCode(currentChar)
     nextCell = `${currentRow}${currentCol}`
     nextCellEl = document.querySelector(`#${currentPlayer} #${nextCell}`)
-    nextCellEl.style.backgroundColor = 'var(--blue)'
+    playerState[nextCell] = 1
+    nextCellEl.classList.add('placed')
   }
+  console.log(valid)
 }
 
 function setBoardState(player, cell) {
