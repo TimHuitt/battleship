2ffda7dd3355def2f81ef33ab0ac306d625b0806
Tim Huitt <timhuitt@gmail.com>
2023-10-28 18:09:30 -0600

    add player ships

--- main.js
+++ main.js
@@ -10,19 +10,20 @@ const playerBoard = []
 
 const opponentBoardEl = document.querySelector("#opponent-board")
 const playerBoardEl = document.querySelector('#player-board')
-const playerShipsContainer = document.querySelectorAll('player-ships-wrapper')
+const playerShips = document.querySelectorAll('#player-ships-wrapper > div')
 
 /*----- event listeners -----*/
 
+playerBoardEl.addEventListener('click', handleClick)
 
 /*----- classes -----*/
 
+// create new cells with the passed id, class, and inner text
 class CreateCell {
-  constructor(col = '', row = '') {
+  constructor(id, cls, text='') {
     this.el = document.createElement('div')
-    this.el.setAttribute('id', row + col)
-    this.el.innerText = row + col
-    this.el.classList.add('cell')
+    this.el.setAttribute('id', id)
+    this.el.classList.add(cls)
     this.el.classList.add('center')
   }
 }
@@ -32,9 +33,11 @@ class CreateCell {
 init()
 
 function init() {
-  renderBoard(opponentBoardEl)
+  // render player
   renderBoard(playerBoardEl)
   renderPlayerShips()
+  // render opponent
+  renderBoard(opponentBoardEl)
 }
 
 function renderPlayerShips() {
@@ -45,9 +48,18 @@ function renderPlayerShips() {
     battleship: 4,
     carrier: 5,
   }
+  let count = 0
   for (const ship in ships) {
+    parent = playerShips[count]
+    id = ship
+    cells = ships[ship]
     
-    console.log(ship, ships[ship])
+    for (let i = 0; i < cells; i++) {
+      const cell = new CreateCell(ship + '_' + id, 'ship')
+      parent.appendChild(cell.el)
+    }
+
+    count++
   }
 }
 
@@ -56,7 +68,7 @@ function renderBoard(element) {
   currentCol = 1
   for (let i = 1; i <= 100; i++) {
     currentRow = String.fromCharCode(row)
-    const cell = new CreateCell(currentCol, currentRow)
+    const cell = new CreateCell(currentRow + currentCol, 'cell', currentRow + currentCol)
     element.appendChild(cell.el)
     if (!(i % 10)) { 
       row++
@@ -64,4 +76,8 @@ function renderBoard(element) {
     }
     currentCol++
   }
+}
+
+function handleClick(e) {
+  console.log(e.target)
 }
